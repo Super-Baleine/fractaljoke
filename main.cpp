b4c3f64b5025292045c55b200bc50d8f725398ef
Super-Baleine <paul.feuvraux@protonmail.ch>
2016-05-15 19:43:07 +0200

    some corrections

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
@@ -7,15 +6,11 @@ int main()
 {
     cout << "Hey ! psst ! Do you know what is a fractal ? ";
     string choice;
-    cin >> choice;
-    if (choice == "no"){
-        int i=0;
-        while (i != 1000000){
-            cout << "\nThis is a fractal :D\n";
-            i++;
-        }
-        return main();
-    }else if (choice == "yes"){
-        cout << "\nOh... so... Bye !";return 0;
-    }else{return main();}
+    for (;;){
+      cout << "Hey ! psst ! Do you know what is a fractal ? ";
+      cin >> choice;
+      if(choice == "yes"){
+        cout << "\nOh... :/";break;return 0;
+      }
+    }
 return 0;}
